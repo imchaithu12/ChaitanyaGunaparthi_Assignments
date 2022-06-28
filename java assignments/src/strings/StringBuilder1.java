e5e921dbb5bf0e3bfeb41f85b03f174e89ec4cdc
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:22:24 +0530

    Code Commited

---
+++ java assignments/src/strings/StringBuilder1.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class StringBuilder1 {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append("String builder, is a peer class of String, that provides much of the, functionality of the string");
+		System.out.println(sb);
+        
+	}
+
+}
