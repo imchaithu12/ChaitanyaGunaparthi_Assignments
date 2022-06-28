0a6ee00a121437e040ceb8bf75feeaeac0899a09
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:21:13 +0530

    Code Commited

---
+++ java assignments/src/strings/StringB3.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class StringB3 {
+
+	public static void main(String[] args) {
+		StringBuffer rev=new StringBuffer("This method returns the reversed object on which it was called");
+        System.out.println(rev);
+        //By using reverse method
+        rev.reverse();
+        System.out.println(rev);
+
+	}
+
+}
