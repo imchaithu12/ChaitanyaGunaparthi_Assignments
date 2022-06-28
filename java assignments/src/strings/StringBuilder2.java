997f8c0dbc4125659b0fd0f2bb6e3b2de2d4eb59
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:23:21 +0530

    Code Commited

---
+++ java assignments/src/strings/StringBuilder2.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class StringBuilder2 {
+
+	public static void main(String[] args) {
+		StringBuilder in=new StringBuilder("It is used to at the specified index");
+        in.insert(14,"insert text ");
+        System.out.println(in);
+	}
+
+}
