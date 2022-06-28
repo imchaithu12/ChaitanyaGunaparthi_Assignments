daaf9da6adc27cb7bdaf349161c9ffc03ab38a23
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:19:55 +0530

    Code Commited

---
+++ java assignments/src/strings/StringB1.java
@@ -0,0 +1,19 @@
+package strings;
+
+public class StringB1 {
+
+	public static void main(String[] args) {
+		    String l="StringBuffer";
+	        String m="is a peer class of String";
+	        String n="that provides much of";
+	        String o="the functionality of strings";
+	        StringBuffer p=new StringBuffer();
+	        
+	        p.append(l);
+	        p.append(" "+m);
+	        p.append(" "+n);
+	        p.append(" "+o);
+	        System.out.println(p);
+	}
+
+}
