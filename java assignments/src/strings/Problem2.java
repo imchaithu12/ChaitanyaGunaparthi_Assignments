e57c56385d08d92cd69f72c75cc8b1c7fe548c1b
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:15:57 +0530

    Code commited

---
+++ java assignments/src/strings/Problem2.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		String a="Hello";
+		String b="How are You?";
+		String c= a + b;
+		System.out.println(c);
+
+	}
+
+}
