5cc0777547c569c364217663d1068bc714826290
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:20:49 +0530

    Code Commited

---
+++ java assignments/src/strings/StringB2.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class StringB2 {
+
+	public static void main(String[] args) {
+		StringBuffer in=new StringBuffer("It is used to at the specified index");
+        in.insert(14,"insert text ");
+        System.out.println(in);
+	}
+
+}
