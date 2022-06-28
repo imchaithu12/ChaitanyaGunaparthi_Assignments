34001b6653891f412f4071aa4953b83bbea74abf
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-06-28 23:14:33 +0530

    Code commited

---
+++ java assignments/src/strings/Problem1.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		String basic= "Hello World";
+		System.out.println("Lenght of the string is "+ basic.length());		
+
+ }
+}
