88c6c336166fdd94656094d638f9808eff7d12ad
guchaita <guchaita@LIN24010865.corp.capgemini.com>
2022-07-02 19:37:59 +0530

    Code Commited

---
+++ java assignments/src/lambda/OrdersQ2.java
@@ -0,0 +1,28 @@
+package lambda;
+
+import java.util.Scanner;
+//import java.util.function.Predicate;
+public class OrdersQ2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the total price of the order");
+        int price=sc.nextInt();
+        System.out.println("Enter the status of your order: 1} Accepted or Completed 2} Ordered");
+        int status= sc.nextInt();
+        acceptance determine =(c,v) -> {
+            if(c>10000 && v==1){
+                System.out.println("Your order of amount "+price+" is accepted and completed");
+            }
+            else if(c<10000){
+                System.out.println("The amount of order is not processable");
+            }
+            else{
+                System.out.println("Your order's status is ambiguous right now");
+            }
+        };
+        determine.decision(price,status);
+    }    
+}
+interface acceptance{
+    void decision(int a,int b);
+}
